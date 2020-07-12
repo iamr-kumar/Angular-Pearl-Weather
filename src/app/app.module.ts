c7278d23df48692a7a6d8f9613dbf8e5b6de19ee
Ritik Kumar <ritik.kumar006@gmail.com>
2020-07-12 18:53:14 +0530

    Added FromsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { WeatherCardComponent } from './weather-card/weather-card.component';
 import { WeatherDetailComponent } from './weather-detail/weather-detail.component';
 import { AppRouterModule } from './app-router.module';
 import { SingleCardComponent } from './weather-card/single-card/single-card.component';
+import { LoadingSpinnerComponent } from './shared/loading-spinner.component';
 
 @NgModule({
   declarations: [
@@ -14,12 +16,13 @@ import { SingleCardComponent } from './weather-card/single-card/single-card.comp
     WeatherCardComponent,
     WeatherDetailComponent,
     SingleCardComponent,
-
+    LoadingSpinnerComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRouterModule
+    AppRouterModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
