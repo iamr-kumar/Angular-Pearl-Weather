b9ba3274cd2641b66f5b88d86d2eab4ac3870da1
Ritik Kumar <ritik.kumar006@gmail.com>
2020-07-17 08:13:53 +0530

    Added Angular Firebase and FireAuth

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBopnFqz67X39SkG5GCw97J5fQQwefGyL4",
+    authDomain: "pearl-weather.firebaseapp.com",
+    databaseURL: "https://pearl-weather.firebaseio.com",
+    projectId: "pearl-weather",
+    storageBucket: "pearl-weather.appspot.com",
+    messagingSenderId: "570640921282",
+    appId: "1:570640921282:web:5fde4dd78097e98ba7015b",
+    measurementId: "G-WTJ4BW0090"
+  }
 };
